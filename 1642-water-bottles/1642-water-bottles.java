c698a325812192142ad9801b4b50548f397f300a
minhhuy <minhhuy5929@gmail.com>
2025-10-01 19:31:58 +0700

    Time: 0 ms (100%), Space: 40.7 MB (31.78%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int result = numBottles;
+        int empty = numBottles;
+        while(empty >= numExchange) {
+            int newBottles = empty / numExchange;
+            result += newBottles;
+            empty = empty % numExchange + newBottles;
+        }
+        return result;
+    }
+}
