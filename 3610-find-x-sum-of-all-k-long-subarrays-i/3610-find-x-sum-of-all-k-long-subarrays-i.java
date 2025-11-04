5959fa2be7d701640bfbdd7ff08a0f62f0825142
minhhuy <minhhuy5929@gmail.com>
2025-11-04 23:39:16 +0700

    Time: 9 ms (78.62%), Space: 47.1 MB (7.59%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.java
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.java
@@ -6,23 +6,19 @@ class Solution {
         int[] ans = new int[n - k + 1];
 
         Map<Integer, Integer> freq = new HashMap<>();
-        // khởi tạo cửa sổ đầu
         for (int i = 0; i < k; i++) {
             freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
         }
         ans[0] = calc(freq, x);
 
-        // trượt cửa sổ
         for (int i = k; i < n; i++) {
             int out = nums[i - k];
             int in  = nums[i];
 
-            // remove out
             int fo = freq.get(out) - 1;
             if (fo == 0) freq.remove(out);
             else freq.put(out, fo);
 
-            // add in
             freq.put(in, freq.getOrDefault(in, 0) + 1);
 
             ans[i - k + 1] = calc(freq, x);
@@ -30,16 +26,14 @@ class Solution {
         return ans;
     }
 
-    // Tính X-Sum từ map tần suất
     private int calc(Map<Integer, Integer> freq, int x) {
-        // gom (val, freq) rồi sort: freq desc, val desc
         List<int[]> list = new ArrayList<>(freq.size());
         for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
             list.add(new int[]{e.getKey(), e.getValue()});
         }
         list.sort((a, b) -> {
-            if (a[1] != b[1]) return b[1] - a[1]; // freq desc
-            return b[0] - a[0];                   // val  desc
+            if (a[1] != b[1]) return b[1] - a[1]; 
+            return b[0] - a[0];                   
         });
 
         long sum = 0;
@@ -47,6 +41,6 @@ class Solution {
         for (int i = 0; i < take; i++) {
             sum += 1L * list.get(i)[0] * list.get(i)[1];
         }
-        return (int) sum; // đề yêu cầu int[]
+        return (int) sum;
     }
 }
